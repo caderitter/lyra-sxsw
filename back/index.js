86d4029e9dd018464c8a66e90c52fb6916d820b8
SamWittmann <smw9@rice.edu>
2019-03-12 23:05:57 -0500

    Added cors

--- back/index.js
+++ back/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 var db = require('./db');
 
-
 const app = express()
 const port = 4000
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 var AuthController = require('./controllers/AuthController');
 // var UserController = require('./controllers/UserController');
