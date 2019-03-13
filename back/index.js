31cb606b48b040e11a8f0c0d0590482564302e8e
SamWittmann <smw9@rice.edu>
2019-03-12 23:10:37 -0500

    cors update

--- back/index.js
+++ back/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 var db = require('./db');
+var cors = require('cors');
 
 const app = express()
 const port = 4000
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors())
 
 var AuthController = require('./controllers/AuthController');
 // var UserController = require('./controllers/UserController');
