8f57c62b4312a2f7d92e8c71ba821cfc6c97f658
caderitter <ritter.cade@gmail.com>
2019-03-12 23:24:59 -0500

    rewrite auth

--- front/src/App.js
+++ front/src/App.js
@@ -4,18 +4,19 @@ import { CookiesProvider } from 'react-cookie';
 
 import SpotifyLoginButton from './components/SpotifyLoginButton';
 import Auth from './components/Auth';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <CookiesProvider>
       <Router>
         <div>
-          <SpotifyLoginButton />
           <Route path="/auth" component={Auth} /> 
           <Route path="/home" />
+          <Route path="/login" component={Login} />
         </div>
       </Router>
-    </CookiesProvider>
+    </CookiesProvider> 
   );
 }
 
--- front/src/components/Auth.js
+++ front/src/components/Auth.js
@@ -1,35 +1,38 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
 import { backendURL } from '../config';
 
-const Auth = (props) => {
-  const [state, setState] = useState("loading");
+export default class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = "loading";
 
+    let params = new URLSearchParams(props.location.search);
 
-  axios.post(`${backendURL}/auth`, {
-    code: props.match.params.code
-  })
-  .then(res => {
-    const { cookies } = props;
-    cookies.set('spotify_token', res.token);
-    setState("done");
-  })
-  .catch(err => {
-    console.log(err);
-    setState("error");
-  });
-
-  switch(state) {
-    case "loading":
-      return <p>Loading...</p>
-    case "done":
-      return <Redirect to="/home" />
-    case "error":
-      return <p>There was an error.</p>
+    axios.post(`${backendURL}/auth`, {
+      code: params.get("code")
+    })
+    .then(res => {
+      const { cookies } = props;
+      cookies.set('spotify_token', res.token);
+      this.state = "done";
+    })
+    .catch(err => {
+      console.log(err);
+      this.state = "error";
+    });
   }
-  
-}
 
-export default Auth;
+  render() {
+    switch(this.state) {
+      case "loading":
+        return <p>Loading...</p>
+      case "done":
+        return <Redirect to="/home" />
+      case "error":
+        return <p>There was an error.</p>
+    }
+  }
+}
