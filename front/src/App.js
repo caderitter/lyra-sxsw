62aef00104660877dc60f41b0f4081e166ad9c23
caderitter <ritter.cade@gmail.com>
2019-03-12 22:51:00 -0500

    fuck

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 import SpotifyLoginButton from './components/SpotifyLoginButton';
+import Auth from './components/Auth';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <SpotifyLoginButton />
-        <Route path="/auth" />
-      </div>
-    </Router>
+    <CookiesProvider>
+      <Router>
+        <div>
+          <SpotifyLoginButton />
+          <Route path="/auth" component={Auth} /> 
+          <Route path="/home" />
+        </div>
+      </Router>
+    </CookiesProvider>
   );
 }
 
-export default App
+export default App;
--- front/src/components/Auth.js
+++ front/src/components/Auth.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import { backendURL } from '../config';
 
-const Auth = () => {
+const Auth = (props) => {
+  const [state, setState] = useState("loading");
+
+
+  axios.post(`${backendURL}/auth`, {
+    code: props.match.params.code
+  })
+  .then(res => {
+    const { cookies } = props;
+    cookies.set('spotify_token', res.token);
+    setState("done");
+  })
+  .catch(err => {
+    console.log(err);
+    setState("error");
+  });
+
+  switch(state) {
+    case "loading":
+      return <p>Loading...</p>
+    case "done":
+      return <Redirect to="/home" />
+    case "error":
+      return <p>There was an error.</p>
+  }
   
 }
+
+export default Auth;
